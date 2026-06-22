b75f8e52765bebcb8f3de5703be91f4acb2d1037
Yeraldyn22 <yeraldinaros48@gmail.com>
2026-06-22 16:11:05 -0500

    Agrego ejercicios practicos a condicionales

---
+++ 03-javascript/condicionales.js
@@ -0,0 +1,34 @@
+let edad = 10; 
+
+if (edad >= 18) {
+   console.log("Eres mayor de edad");
+} else {
+   console.log("Eres menor de edad");
+}
+
+let nota = 2.5; 
+
+if(nota <= 3.0) {
+    console.log("perdiste la materia");
+}else {
+    console.log("Ganaste la materia")
+}
+
+let dinero = 18000;
+
+if(dinero >= 15000) {
+    console.log("Si te alcanza para una fruta");
+}else {
+    console.log("Solo te alcanza para un dulce")
+}
+
+let temperatura = 21;
+
+if(temperatura <= 22) {
+    console.log("El clima esta fresco")
+}else {
+    console.log("El clima esta caloroso")
+}
+
+
+
