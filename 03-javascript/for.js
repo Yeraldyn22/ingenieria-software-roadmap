e7b647ba26777da085a81292329d7b3739226d0c
Yeraldyn22 <yeraldinaros48@gmail.com>
2026-06-22 17:13:58 -0500

    Agrego ejercicios practicos a for.js

---
+++ 03-javascript/for.js
@@ -0,0 +1,16 @@
+/*for (let i = 20; i >= 2; i-=2){
+    console.log(i);
+}
+
+for (let i = 30; i >= 5; i-=5){
+    console.log(i)
+}
+
+for (let i = 50; i>=15; i-=4){
+    console.log(i)
+}
+*/
+
+for (let i = 10; i<=50; i+=2){
+    console.log(i)
+}
