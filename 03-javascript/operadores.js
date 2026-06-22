bed93484669adde7b351cb2a98eafa0f8e403c57
Yeraldyn22 <yeraldinaros48@gmail.com>
2026-06-21 21:16:23 -0500

    Agrego ejercicio operadores.js

---
+++ 03-javascript/operadores.js
@@ -0,0 +1,7 @@
+let num1 = 54;
+let num2 = 22;
+
+console.log("Suma:", num1 + num2);
+console.log("Resta:", num1 -  num2);
+console.log("Multiplicacion", num1 * num2);
+console.log("Division", num1 / num2);
