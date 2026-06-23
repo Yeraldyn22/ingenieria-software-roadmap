892bddd4cef0ac90b064d36497dd19ba3846e72a
Yeraldyn22 <yeraldinaros48@gmail.com>
2026-06-23 15:31:31 -0500

    Agrego ejercicios practicos a objetos.js

---
+++ 03-javascript/objetos.js
@@ -0,0 +1,42 @@
+/*let persona = {
+    nombre: "yeraldyn",
+    edad: 21,
+    ciudad:"Neiva",
+};
+
+console.log(persona.nombre);
+console.log(persona.edad);
+console.log(persona.ciudad);
+
+let persona = {
+    nombre: "yeraldyn",
+    edad: 21,
+    ciudad:"Neiva",
+};
+
+console.log(persona.nombre);
+console.log(persona.edad);
+console.log(persona.ciudad);*/
+
+let persona = {
+    nombre: "yeraldyn",
+    edad: 21,
+    ciudad: "Neiva",
+    carrera: "Ing de Software",
+    colorFavorito:"blue"
+};
+
+console.log(persona.nombre);
+console.log(persona.carrera);
+console.log(persona.colorFavorito);
+
+persona.edad = 22;
+
+console.log(persona.edad);
+
+persona.ciudad = "Medellin";
+persona.colorFavorito = "white";
+
+console.log(persona.nombre);
+console.log(persona.ciudad);
+console.log(persona.colorFavorito);
