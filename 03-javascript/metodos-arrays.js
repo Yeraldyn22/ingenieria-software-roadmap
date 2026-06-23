99e3c1622583888d4b0899dd3b3afc66d53af09f
Yeraldyn22 <yeraldinaros48@gmail.com>
2026-06-23 17:29:28 -0500

    Agrego ejercicios practicos en metodos-arrays.js

---
+++ 03-javascript/metodos-arrays.js
@@ -0,0 +1,51 @@
+//Metodo 1. push agrega un elemento al final de array
+
+/*let frutas = ["Banano", "Manzana"];
+
+frutas.push("pera");
+
+console.log(frutas);
+
+//Metodo 2. pop() eliminar el último elemento
+
+let frutas = ["Banano", "Manzana"];
+
+frutas.push("pera");
+
+console.log(frutas);
+
+frutas.pop();
+
+console.log(frutas);
+
+//Metodo 3. legth() contar cuántos elementos tiene el array.
+
+let frutas = ["Banano", "Manzana"];
+
+frutas.push("pera");
+
+console.log(frutas);
+
+frutas.pop();
+
+console.log(frutas);
+
+console.log(frutas.length);
+
+let colores = ["Rojos", "Azul", "verde", "Amarillo", " Negro"];
+
+console.log(colores.length); *
+
+//Metodo 4. includes() responde una pregunta: "¿Existe este elemento dentro del array?"
+
+let frutas = ["Banano", "Manzana", "Pera"];
+
+console.log(frutas.includes("Pera"));
+console.log(frutas.includes("Mango")); */
+
+let nombres = ["Yeraldyn", "Angela", "Maria"];
+
+console.log(nombres.includes("Maria"));
+console.log(nombres.includes("Alison"));
+
+
