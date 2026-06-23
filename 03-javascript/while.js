e4789c21ad4db5f28e0c4b0b3699259161d1e15c
Yeraldyn22 <yeraldinaros48@gmail.com>
2026-06-23 15:05:31 -0500

    Agrego ejercicia de while.js

---
+++ 03-javascript/while.js
@@ -0,0 +1,20 @@
+/*let contador = 1;
+
+while (contador <= 5) {
+    console.log(contador);
+    contador++;
+}*/
+
+/*let numero = 10;
+
+while (numero >=2) {
+    console.log(numero);
+    numero -=2;
+}*/
+
+let edad =5;
+
+while (edad <=17) {
+    console.log(edad);
+    edad +=4
+}
