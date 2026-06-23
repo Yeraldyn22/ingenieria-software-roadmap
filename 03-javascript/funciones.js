0258b5fb5442f5bd8a3dac4d1f6389cd08ae9d70
Yeraldyn22 <yeraldinaros48@gmail.com>
2026-06-22 22:36:15 -0500

    Agrego ejercicios practicos en funciones.js

---
+++ 03-javascript/funciones.js
@@ -0,0 +1,29 @@
+/*function saludar() {
+    console.log("Hola Yeraldyn")
+}
+saludar();
+
+function saludar() {
+    console.log("Hola Yeraldyn")
+}
+saludar();
+saludar();
+saludar();
+
+//Parametros
+
+function saludar(nombre) {
+    console.log("Hola " + nombre)
+}
+
+saludar("Yeraldyn");
+saludar("Angela");
+saludar("Maria");
+saludar("Alison");*/
+
+function multiplicar(num1, num2){
+    console.log(num1 * num2)
+}
+
+multiplicar(6, 9)
+multiplicar(10, 10)
