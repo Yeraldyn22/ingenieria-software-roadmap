9276ed69557d99e0059296b5257399680f8bbfb1
Yeraldyn22 <yeraldinaros48@gmail.com>
2026-06-22 23:26:50 -0500

    Agrego ejercicios practicos en Arrays.js

---
+++ 03-javascript/arrays.js
@@ -0,0 +1,28 @@
+/*let nombres =["Yeraldyn", "Angela", "Maria", "Alison"];
+
+console.log(nombres);
+
+let nombres =["Yeraldyn", "Angela", "Maria", "Alison"];
+
+console.log(nombres[0]);
+console.log(nombres[1]);
+console.log(nombres[2]);
+console.log(nombres[3]);
+
+//Mezcla de texto con  Arrays
+
+let nombres =["Yeraldyn", "Angela", "Maria", "Alison"];
+
+console.log("Mi nombre es " + nombres [0]);
+console.log("Mi madrina y prima es " + nombres[1]);
+console.log(nombres [2], "es su segundo nombre y");
+console.log("la hija de ella es mi prima tambien y su nombre es " + nombres[3]);*/
+
+let colores = ["Red", "Blue"];
+
+colores.push("green");
+colores.push("Yellow");
+
+console.log(colores[0]);
+console.log(colores[2]);
+console.log(colores[3]);
