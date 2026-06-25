eb091f10aa63720dc0a0d182b4e0ee9acfb5b5d3
Yeraldyn22 <yeraldinaros48@gmail.com>
2026-06-25 13:15:01 -0500

    Agrego ejercicios practicos a arrays-for.js

---
+++ 03-javascript/arrays-for.js
@@ -0,0 +1,36 @@
+/*let nombres = ["Yeraldyn", "Angela", "Maria", "Alison"];
+
+for (let i = 0; i < nombres.length; i++){
+    
+    console.log(nombres[i]);
+
+}
+
+let frutas = ["Banano", "Manzana", "Pera"];
+
+for (let i = 0; i< frutas.length; i++){
+    
+    console.log(frutas[i]);
+}
+
+let ciudades = ["Neiva", "Medellin", "Bogota", "Cali"];
+
+for (let i = 0; i< ciudades.length; i++){
+
+    console.log(ciudades[i]);
+}
+
+let utilesEscolares = ["Lapiz", "borrador", "Sacapunta", "Corrector"];
+
+for (let i = 0; i < utilesEscolares.length; i++){
+   
+    console.log(utilesEscolares[i]);
+}*/
+
+let numeros = [10, 20, 30, 40, 50];
+
+for (let i= 0; i < numeros.length; i++){
+
+    console.log(numeros[i]);
+}
+
