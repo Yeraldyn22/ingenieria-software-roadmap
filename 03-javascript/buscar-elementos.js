59d9cf885e09b2c58f33b904ad2b0ed7da2e4f87
Yeraldyn22 <yeraldinaros48@gmail.com>
2026-06-26 22:16:01 -0500

    Agrego ejercicios practicos a biscar-elemnetos.js

---
+++ 03-javascript/buscar-elementos.js
@@ -0,0 +1,15 @@
+/*let nombres = ["Yeraldyn", "Angela", "Maria", "Alison"];
+
+console.log(nombres.includes("Maria"));
+console.log(nombres.includes("Pedro"));
+
+let colores = ["Azul", "Rojo", "Verde", "Amarillo"];
+
+console.log(colores.includes("Verde"));
+console.log(colores.includes("Negro"));*/
+
+
+let grupoSisben = ["A", "B", "C"];
+
+console.log(grupoSisben.includes("D"));
+console.log(grupoSisben.includes("C"));
